2b678ec7641be1818b17aef9c78981aeddbb6775
1053226965@qq.com <1053226965@qq.com>
2023-06-05 17:57:13 +0800

    log2

---
+++ src/server/utils/math.go
@@ -0,0 +1,24 @@
+package utils
+
+func GetPow2(hint int) int {
+	hint--
+	hint |= (hint >> 1)
+	hint |= (hint >> 2)
+	hint |= (hint >> 4)
+	hint |= (hint >> 8)
+	hint |= (hint >> 16)
+	hint++
+	return hint
+}
+
+func GetPow2_64(hint int64) int64 {
+	hint--
+	hint |= (hint >> 1)
+	hint |= (hint >> 2)
+	hint |= (hint >> 4)
+	hint |= (hint >> 8)
+	hint |= (hint >> 16)
+	hint |= (hint >> 32)
+	hint++
+	return hint
+}
