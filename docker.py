3b4d4ff91c2a94c5d253f67f3308441a7fa1b73e
1053226965@qq.com <1053226965@qq.com>
2023-10-19 11:24:25 +0800

    new line error

--- docker.py
+++ docker.py
@@ -29,12 +29,12 @@ def gen_sn():
   f.close()
   if len(git_proxy) != 0:
     dc.replace("#git_proxy", "RUN git config --global http.proxy={0}".format(git_proxy))
-  dc += "/nCOPY {0} /app".format(server_config)
-  dc += "/nCOPY {0} /app".format(bt_config)
-  dc += "/nCOPY {0}/http.crt /app/tls/".format(tls_config)
-  dc += "/nCOPY {0}/http.key /app/tls/".format(tls_config)
-  dc += "/nCOPY {0}/rpc.crt /app/tls/".format(tls_config)
-  dc += "/nCOPY {0}/rpc.key /app/tls/".format(tls_config)
+  dc += "\nCOPY {0} /app".format(server_config)
+  dc += "\nCOPY {0} /app".format(bt_config)
+  dc += "\nCOPY {0}/http.crt /app/tls/".format(tls_config)
+  dc += "\nCOPY {0}/http.key /app/tls/".format(tls_config)
+  dc += "\nCOPY {0}/rpc.crt /app/tls/".format(tls_config)
+  dc += "\nCOPY {0}/rpc.key /app/tls/".format(tls_config)
   gen_docker_image(dc, "sn")
 
 def main():
