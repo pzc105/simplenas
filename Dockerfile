83a2fc6b072f9b4f0c364d576b44388b449c41e0
1053226965@qq.com <1053226965@qq.com>
2023-10-19 11:01:21 +0800

    config handle

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM myenv
-RUN mkdir -p /app
-# RUN git config --global http.proxy socks5://192.168.10.241:10808
+RUN mkdir -p /app && mkdir -p /app/tls
+#git_proxy
 RUN mkdir -p /source
 RUN git clone --recursive https://github.com/pzc105/simplenas.git /source/simplenas && \
     cd /source/simplenas && \
@@ -8,10 +8,10 @@ RUN git clone --recursive https://github.com/pzc105/simplenas.git /source/simple
     cd cmake/build && \
     cmake ../.. -Dstatic_runtime=true && \
     make -j$(nproc) && \
-    cp ./bin/bt /app/bt/bt
+    cp ./bin/bt /app/bt
 RUN cd /source/simplenas/src/server && \
     go build && \
-    cp ./pnas /app/pnas/pnas
+    cp ./pnas /app/pnas
 RUN npm install -g pnpm && \
     cd /source/simplenas/src/frontend && \
     npm i && npm build
---
+++ docker.py
@@ -0,0 +1,63 @@
+import os, sys, getopt
+
+git_proxy = ""
+server_config = "./server.yml"
+bt_config = "./bt.yml"
+tls_config = "./tls"
+
+def gen_docker_image(content, image_name):
+  tmp_dir = "./tmp"
+  if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+  f = open(tmp_dir + "/Dockerfile", "w")
+  f.write(content)
+  f.close()
+  os.system("sudo docker image built -t {0} {1}".format(image_name, tmp_dir))
+
+
+def gen_myenv():
+  f = open("./myenv/Dockerfile")
+  dc = f.read()
+  f.close()
+  if len(git_proxy) != 0:
+    dc.replace("#git_proxy", "RUN git config --global http.proxy={0}".format(git_proxy))
+  gen_docker_image(dc, "myenv")
+
+def gen_sn():
+  f = open("./Dockerfile")
+  dc = f.read()
+  f.close()
+  if len(git_proxy) != 0:
+    dc.replace("#git_proxy", "RUN git config --global http.proxy={0}".format(git_proxy))
+  dc += "/nCOPY {0} /app".format(server_config)
+  dc += "/nCOPY {0} /app".format(bt_config)
+  dc += "/nCOPY {0}/http.crt /app/tls/".format(tls_config)
+  dc += "/nCOPY {0}/http.key /app/tls/".format(tls_config)
+  dc += "/nCOPY {0}/rpc.crt /app/tls/".format(tls_config)
+  dc += "/nCOPY {0}/rpc.key /app/tls/".format(tls_config)
+  gen_docker_image(dc, "sn")
+
+def main():
+  global git_proxy
+  global server_config
+  global bt_config
+  global tls_config
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["git_proxy=", "server_config=", "bt_config=", "tls_config="])
+  except getopt.GetoptError:
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '--git_proxy':
+      git_proxy = arg
+    elif opt == '--server_config':
+      server_config = arg
+    elif opt == '--bt_config':
+      bt_config = arg
+    elif opt == '--tls_config':
+      tls_config = arg
+  
+  gen_myenv()
+  gen_sn()
+
+if __name__ == "__main__":
+  main()
