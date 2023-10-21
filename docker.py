dcefba7edebb2fdf1fa539003e09b69e79eec5c7
dada105.hero@gmail.com <dada105.hero@gmail.com>
2023-10-22 00:24:23 +0800

    o

--- docker.py
+++ docker.py
@@ -79,11 +79,11 @@ def main():
     os.system("sudo docker exec {0} /bin/bash -c 'service mysql start && service redis-server start'".format(container_name))
     os.system("sudo docker exec {0} /bin/bash -c 'mkdir -p /app/media/poster && \
                 cp /source/simplenas/default_folder.png /app/media/poster/ && \
-                cp /source/simplenas/house.png /app/media/poster/'")
+                cp /source/simplenas/house.png /app/media/poster/'".format(container_name))
     os.system("sudo docker exec {0} /bin/bash -c '\
   while ! mysqladmin ping -h\"127.0.0.1\" --silent; do \
       sleep 1\
-  done'")
+  done'".format(container_name))
     os.system("sudo docker exec {0} /bin/bash -c 'mysql -uroot -p123 < /source/simplenas/src/server/tables.sql'".format(container_name))
     os.system("sudo docker exec {0} /bin/bash -c \"cd /app && (nohup ./bt &) && (nohup ./pnas &)\"".format(container_name))
     os.system("sudo docker exec {0} /bin/bash -c \"echo 'REACT_APP_RPC_SERVER=https://rpc.pnas105.top:11236' > /source/simplenas/src/frontend/.env.local\"".format(container_name))
