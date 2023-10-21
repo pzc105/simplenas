3869ec73ef693a7f3c37173eee8641e18cd990d3
dada105.hero@gmail.com <dada105.hero@gmail.com>
2023-10-21 17:54:10 +0800

    other

--- docker.py
+++ docker.py
@@ -37,11 +37,12 @@ def main():
   global bt_config
   global tls_config
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "r:i", ["git_proxy=", "server_config=", "bt_config=", "tls_config=", "build_myenv="])
+    opts, args = getopt.getopt(sys.argv[1:], "rn:i", ["git_proxy=", "server_config=", "bt_config=", "tls_config=", "build_myenv="])
   except getopt.GetoptError:
     sys.exit(2)
   build_myenv = True
-  run_container = ""
+  run_container = False
+  container_name = ""
   init_container = False
   for opt, arg in opts:
     if opt == '--git_proxy':
@@ -54,28 +55,30 @@ def main():
       tls_config = arg
     elif opt == '--build_myenv' and (arg.lower() == "false" or arg.lower() == "0"):
       build_myenv = False
-    elif opt == '-r':
-      run_container = arg
-    elif opt == '-i' and (arg.lower() == "false" or arg.lower() == "0"):
+    elif opt == '-r' and (arg.lower() == "true" or arg.lower() != "0"):
+      run_container = True
+    elif opt == '-n':
+      container_name = arg
+    elif opt == '-i' and (arg.lower() == "true" or arg.lower() != "0"):
       init_container = True
   if build_myenv:
     gen_myenv()
   gen_sn()
 
   if len(run_container) > 0:
-    os.system("sudo docker run -p 3000:3000 -p 6881:6881 -p 22345:22345 -p 11236:11236 --name sn -dti sn")
+    os.system("sudo docker run -p 3000:3000 -p 6881:6881 -p 22345:22345 -p 11236:11236 --name {0} -dti sn".format(container_name))
   
   if init_container:
-    os.system("sudo docker cp {0} sn:/app".format(server_config))
-    os.system("sudo docker cp {0} sn:/app".format(bt_config))
-    os.system("sudo docker cp {0}/http.crt sn:/app/tls".format(tls_config))
-    os.system("sudo docker cp {0}/http.key sn:/app/tls".format(tls_config))
-    os.system("sudo docker cp {0}/rpc.crt sn:/app/tls".format(tls_config))
-    os.system("sudo docker cp {0}/rpc.key sn:/app/tls".format(tls_config))
-    os.system("sudo docker exec -it sn sh -c 'service mysql start && service redis-server start'")
-    os.system("sudo docker exec -it sn sh -c 'cd /app && ./bt &'")
-    os.system("sudo docker exec -it sn sh -c 'cd /app && ./pnas &'")
-    os.system("sudo docker exec -it sn sh -c 'cd /source/simplenas/src/frontend && chmod +x start_unix.sh && ./start_unix.sh -c /app/tls/http.crt -k /app/tls/http.key &'")
+    os.system("sudo docker cp {0} {1}:/app".format(server_config, container_name))
+    os.system("sudo docker cp {0} {1}:/app".format(bt_config, container_name))
+    os.system("sudo docker cp {0}/http.crt {1}:/app/tls".format(tls_config, container_name))
+    os.system("sudo docker cp {0}/http.key {1}:/app/tls".format(tls_config, container_name))
+    os.system("sudo docker cp {0}/rpc.crt {1}:/app/tls".format(tls_config, container_name))
+    os.system("sudo docker cp {0}/rpc.key {1}:/app/tls".format(tls_config, container_name))
+    os.system("sudo docker exec -it {0} sh -c 'service mysql start && service redis-server start'".format(container_name))
+    os.system("sudo docker exec -it {0} sh -c 'cd /app && ./bt &'".format(container_name))
+    os.system("sudo docker exec -it {0} sh -c 'cd /app && ./pnas &'".format(container_name))
+    os.system("sudo docker exec -it {0} sh -c 'cd /source/simplenas/src/frontend && chmod +x start_unix.sh && ./start_unix.sh -c /app/tls/http.crt -k /app/tls/http.key &'".format(container_name))
 
 
 if __name__ == "__main__":
